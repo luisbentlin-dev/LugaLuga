98d24896ac24a8049ad2a7d0e5184580a2eb2f20
Luis Bentlin <luis.bentlin@aluno.senai.br>
2024-05-28 16:41:37 -0300

    Aula 28/05

--- app/src/main/java/com/example/lugaluga/view/FalaFragment.java
+++ app/src/main/java/com/example/lugaluga/view/FalaFragment.java
@@ -4,41 +4,35 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lugaluga.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FalaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class FalaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private TextInputLayout inputEmail;
     public FalaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FalaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
+
     public static FalaFragment newInstance(String param1, String param2) {
         FalaFragment fragment = new FalaFragment();
         Bundle args = new Bundle();
@@ -48,19 +42,56 @@ public class FalaFragment extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fala, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_fala, container, false);
+
+        inputEmail = view.findViewById(R.id.input_email);
+
+        inputEmail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Pattern pattern;
+                Matcher matcher;
+                String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                pattern = Pattern.compile(EMAIL_PATTERN);
+                CharSequence cs = (CharSequence) s;
+                matcher = pattern.matcher(cs);
+                if (!(matcher.matches() == true)) {
+                    inputEmail.setError("Invalid email");
+                } else {
+                    inputEmail.setError("");
+                }
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/lugaluga/view/cadastrousuarioActivity.java
+++ app/src/main/java/com/example/lugaluga/view/cadastrousuarioActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 
 import com.example.lugaluga.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class cadastrousuarioActivity extends AppCompatActivity {
 
     private Spinner spinneruf;
+
+    private TextInputLayout inputEmail;
+
+    private TextInputLayout input_cpf;
+
+    private TextInputLayout input_CEP;
+    private TextInputLayout input_DataNasc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,59 @@ public class cadastrousuarioActivity extends AppCompatActivity {
         myToolbar.setTitle("cadastro");
         setSupportActionBar(myToolbar);
 
+        input_cpf = findViewById(R.id.input_cpf);
+
+        input_cpf.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskCPF = "###.###.###-##";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*", "");
+                String mask = maskCPF;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                input_cpf.getEditText().setText(mascara);
+                input_cpf.getEditText().setSelection(mascara.length());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         spinneruf = (Spinner) findViewById(R.id.spinner_uf);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -32,5 +98,139 @@ public class cadastrousuarioActivity extends AppCompatActivity {
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinneruf.setAdapter(adapter);
+
+        inputEmail = findViewById(R.id.input_email);
+
+        inputEmail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Pattern pattern;
+                Matcher matcher;
+                String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                pattern = Pattern.compile(EMAIL_PATTERN);
+                CharSequence cs = (CharSequence) s;
+                matcher = pattern.matcher(cs);
+                if (!(matcher.matches() == true)) {
+                    inputEmail.setError("Invalid email");
+                } else {
+                    inputEmail.setError("");
+                }
+            }
+        });
+
+        input_CEP = findViewById(R.id.input_cep);
+
+        input_CEP.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskCEP = "#####-###";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*", "");
+                String mask = maskCEP;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                input_CEP.getEditText().setText(mascara);
+                input_CEP.getEditText().setSelection(mascara.length());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        input_DataNasc = findViewById(R.id.input_data);
+
+        input_DataNasc.getEditText().addTextChangedListener(new TextWatcher() {
+
+            private static final String maskDATANASC = "##/##/####";
+
+            boolean isUpdating;
+
+            String old = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString().replaceAll("[^0-9]*", "");
+                String mask = maskDATANASC;
+
+                String mascara = "";
+                if (isUpdating){
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if ((m != '#' && str.length() > old.length()) || (m != '#' && str.length() < old.length() && str.length() != i)) {
+                        mascara += m;
+                        continue;
+                    }
+
+
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                input_DataNasc.getEditText().setText(mascara);
+                input_DataNasc.getEditText().setSelection(mascara.length());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
